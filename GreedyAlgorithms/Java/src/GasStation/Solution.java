1d3921729ae9fda37a9f0ce65339a423b07ff302
Yuka <yuka.0531.a@gmail.com>
2020-03-10 15:43:50 -0700

    Add a package name

--- GreedyAlgorithms/Java/src/GasStation/Solution.java
+++ GreedyAlgorithms/Java/src/GasStation/Solution.java
@@ -1,3 +1,5 @@
+package GasStation;
+
 /* 134. Gas Station
  * There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
  * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to
